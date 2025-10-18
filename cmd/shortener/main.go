e485ad242d1c540eae6bc82ab649032984cd1009
pda windows <rebaxis@gmail.com>
2025-10-18 13:25:21 +0300

    fix bug

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -5,6 +5,7 @@ import (
 
 	chi "github.com/go-chi/chi/v5"
 	"github.com/rebaxis/urlshrter/internal/handler/create"
+	"github.com/rebaxis/urlshrter/internal/handler/get"
 )
 
 // define storage variable
@@ -13,7 +14,7 @@ var urlS = map[string]string{}
 func main() {
 	r := chi.NewRouter()
 	r.Post("/", create.ScreateID(urlS))
-	r.Get("/{id}", create.ScreateID(urlS))
+	r.Get("/{id}", get.GetURLByID(urlS))
 
 	if err := http.ListenAndServe(`:8080`, r); err != nil {
 		panic(err)
