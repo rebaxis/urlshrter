6b26fed2b37cf1ab760b37138ff96ff70ce8dd67
pda windows <rebaxis@gmail.com>
2025-10-26 15:14:22 +0300

    new test were added

--- internal/handler/create/create_test.go
+++ internal/handler/create/create_test.go
@@ -26,8 +26,7 @@ func TestCreateID(t *testing.T) {
 		response string
 	}
 	tests := []struct {
-		name string // description of this test case
-		// Named input parameters for target function.
+		name      string
 		requestTo requestTo
 		want      want
 	}{
